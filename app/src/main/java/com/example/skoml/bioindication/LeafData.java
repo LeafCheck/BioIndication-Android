98a79901174d3baddc1ae31b8a7e85d994ac3645
AlexT <tischenkoalex@gmail.com>
2016-05-19 20:12:29 +0300

    in progress

---
+++ app/src/main/java/com/example/skoml/bioindication/LeafData.java
@@ -0,0 +1,27 @@
+package com.example.skoml.bioindication;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Developer on 5/19/2016.
+ */
+public class LeafData {
+
+    private Bitmap leafImage;
+
+    public LeafData(Bitmap leafImage) {
+        this.leafImage = leafImage;
+    }
+
+    public Bitmap getImage() {
+        return leafImage;
+    }
+
+    public int getWidth() {
+        return leafImage.getWidth();
+    }
+
+    public int getHeight() {
+        return leafImage.getHeight();
+    }
+}
